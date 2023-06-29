f3dee127b2bf29e68e5ae6d01197687889048385
João Damaceno <joaodamaceno188@gmail.com>
2023-06-29 17:27:47 -0400

    feat: :sparkles: adiciona trait de factory.

--- app/Models/Profissional.php
+++ app/Models/Profissional.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Profissional extends Model
 {
+    use HasFactory;
     //Seta a model para o nome da tabela.
     protected $table = 'pron_profissional_de_saude';
     protected $guarded = [];
