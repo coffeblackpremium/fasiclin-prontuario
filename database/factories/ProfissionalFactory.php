8e694c1b5a4d80418682d9dd4b984d4e35429276
João Damaceno <joaodamaceno188@gmail.com>
2023-06-29 15:00:03 -0400

    feat: :sparkles: factory de profissional.

---
+++ database/factories/ProfissionalFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Profissional;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProfissionalFactory extends Factory
+{
+    protected $model = Profissional::class;
+
+    public function definition(): array
+    {
+        return [
+            'id_profissional' => fake()->unique()->numberBetween(1,9),
+            'nome' => fake()->name,
+            'especialidade' => fake()->word,
+        ];
+    }
+}
