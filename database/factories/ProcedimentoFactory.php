cf5bb12627d5a4d68958e0f2073ebb5706cc99d4
João Damaceno <joaodamaceno188@gmail.com>
2023-06-29 14:38:21 -0400

    feat: :sparkles: factory de procedimento.

---
+++ database/factories/ProcedimentoFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Procedimentos;
+use App\Models\Profissional;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProcedimentoFactory extends Factory
+{
+    protected $model = Procedimentos::class;
+
+    public function definition(): array
+    {
+        return [
+            'id_procedimento' => fake()->numberBetween(1,9),
+            'id_profissional' => Profissional::query()->inRandomOrder()->first()->id,
+            'procedimento' => fake()->sentence(),
+            'custo_honorario' => fake()->randomFloat('2'),
+            'honorarios' => fake()->randomFloat('2')
+        ];
+    }
+}
