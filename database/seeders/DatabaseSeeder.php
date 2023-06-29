5b2ef52c3722840e36b304843baf2d2a09a965f8
João Damaceno <joaodamaceno188@gmail.com>
2023-06-29 18:01:23 -0400

    refactor: :recycle: adiciona seeders para as tabelas.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+        \App\Models\Profissional::factory()->count(5)->create();
+        \App\Models\Fornecedor::factory()->count(5)->create();
+        \App\Models\Procedimento::factory()->count(5)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
