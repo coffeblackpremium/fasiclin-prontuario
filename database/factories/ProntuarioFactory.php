12380a40c91afd3cf9901cf1f5bef42d3a37940f
João Damaceno <joaodamaceno188@gmail.com>
2023-06-29 17:27:26 -0400

    feat: :sparkles: factory de prontuario.

---
+++ database/factories/ProntuarioFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Cliente;
+use App\Models\Especialidade;
+use App\Models\Procedimentos;
+use App\Models\Profissional;
+use App\Models\Prontuario;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProntuarioFactory extends Factory
+{
+    protected $model = Prontuario::class;
+
+    public function definition(): array
+    {
+        return [
+            'id_cpf' => Cliente::query()->inRandomOrder()->first()->cpf,
+            'id_especialidade' => Especialidade::query()->inRandomOrder()->first()->id,
+            'id_procedimentos' => Procedimentos::query()->inRandomOrder()->first()->id,
+            'id_profissional_de_saude' => Profissional::query()->inRandomOrder()->first()->id,
+            'data_hora' => fake()->unique()->date(),
+            'descricao' => fake()->paragraph(2),
+            'link' => fake()->unique()->file(),
+            'autenticado' => fake()->boolean(50),
+        ];
+    }
+}
