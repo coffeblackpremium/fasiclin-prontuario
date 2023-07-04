ba128735a1b3d83d235fb74339483a671e4e6a32
João Damaceno <joaodamaceno188@gmail.com>
2023-07-04 08:31:38 -0400

    refactor: :recycle: faz relacionamento de cliente.

--- app/Models/Prontuario.php
+++ app/Models/Prontuario.php
@@ -4,14 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Prontuario extends Model
 {
     use HasFactory;
 
-    protected $table = 'pron_prontuario';
+    protected $table = 'pron_prontuarios';
     protected $guarded = [];
 
     const CREATED_AT = null;
     const UPDATED_AT = null;
+
+    public function cliente(): HasMany
+    {
+        return $this->hasMany(Cliente::class);
+    }
 }
