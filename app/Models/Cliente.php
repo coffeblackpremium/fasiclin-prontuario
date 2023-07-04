9925a67056035e30e94406ea604c4098701b0d64
João Damaceno <joaodamaceno188@gmail.com>
2023-07-04 08:30:10 -0400

    refactor: :lipstick: relacionamento de prontuario e cliente.

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Cliente extends Model
 {
@@ -12,4 +13,9 @@ class Cliente extends Model
     protected $table = 'ag_cliente';
 
     protected $guarded = [];
+
+    public function prontuario(): BelongsTo
+    {
+        return $this->belongsTo(Prontuario::class);
+    }
 }
