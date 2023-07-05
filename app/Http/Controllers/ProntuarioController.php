9923626c959d10b6884706f8301c1da05606b709
João Damaceno <joaodamaceno188@gmail.com>
2023-07-05 17:15:35 -0400

    patch: :recycle: inner join com tabelas e adiciona as informações .

--- app/Http/Controllers/ProntuarioController.php
+++ app/Http/Controllers/ProntuarioController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Cliente;
 use App\Models\Prontuario;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
@@ -23,6 +24,7 @@ class ProntuarioController extends Controller
             ->join('ag_cliente', 'ag_cliente.cpf_cliente', '=', 'pron_prontuarios.cpf_cliente')
             ->join('pron_procedimentos', 'pron_procedimentos.id_procedimento', '=', 'pron_prontuarios.id_procedimentos')
             ->join('pron_especialidade', 'pron_especialidade.id_especialidade', '=', 'pron_prontuarios.id_especialidade')
+            ->orderBy('pron_prontuarios.data_abertura', 'desc')
             ->select('pron_prontuarios.*', 'ag_cliente.nome_cliente', 'pron_procedimentos.procedimento', 'pron_especialidade.nome')
             ->get();
 
@@ -43,8 +45,26 @@ class ProntuarioController extends Controller
 
     public function edit($cpfCliente, $dataAbertura): Response
     {
-        $prontuario = Prontuario::where('cpf_cliente', $cpfCliente)->where('data_abertura', $dataAbertura)->first();
-        return Inertia::render('Prontuario/Edit', ['prontuario' => $prontuario]);
+        $data = Carbon::parse($dataAbertura)->format('Y-m-d H:i:s');
+
+        $prontuario = DB::table('pron_prontuarios')
+            ->where('pron_prontuarios.cpf_cliente', $cpfCliente)
+            ->where('pron_prontuarios.data_abertura', $data)
+            ->join('ag_cliente', 'ag_cliente.cpf_cliente', '=', 'pron_prontuarios.cpf_cliente')
+            ->join('pron_procedimentos', 'pron_procedimentos.id_procedimento', '=', 'pron_prontuarios.id_procedimentos')
+            ->join('pron_especialidade', 'pron_especialidade.id_especialidade', '=', 'pron_prontuarios.id_especialidade')
+            ->join('pron_profissional', 'pron_profissional.id_profissional_de_saude', '=', 'pron_prontuarios.id_profissional_de_saude')
+            ->select(
+                'pron_prontuarios.*',
+                'ag_cliente.nome_cliente',
+                'pron_procedimentos.procedimento',
+                'pron_especialidade.nome as nome_especialidade',
+                'pron_profissional.nome as nome_profissional')
+            ->first();
+
+        return Inertia::render('Prontuario/Edit', [
+            'prontuario' => $prontuario,
+        ]);
     }
 
     public function update(Request $request, $id)
--- resources/js/Pages/Prontuario/Edit.jsx
+++ resources/js/Pages/Prontuario/Edit.jsx
@@ -7,40 +7,41 @@ import PrimaryButton from "@/Components/PrimaryButton.jsx";
 import {usePage} from "@inertiajs/react";
 import {Navbar} from "@/Components/Navbar.jsx";
 
-export default function Edit({prontuario, cliente}) {
+export default function Edit({prontuario}) {
+    console.log(prontuario);
     return (
         <div className={"md:h-screen bg-green-600 dark:bg-gray-900"}>
             <Navbar />
             <div className="mx-auto pt-24">
-                <h2 className="text-3xl text-center mb-4 font-bold dark:text-white">Prontuário do Paciente: {cliente.nome_cliente}</h2>
+                <h2 className="text-3xl text-center mb-4 font-bold dark:text-white">Prontuário do Paciente: {prontuario.nome_cliente}</h2>
             </div>
             <div className="bg-white dark:bg-gray-700 mx-auto md:w-full max-w-7xl py-11 px-10 shadow rounded-lg">
                 <form action="" className={"mt-5 md:mt-0"}>
                     <div className="md:grid md:grid-cols-2 md:gap-3 md:content-start flex flex-col justify-center">
                         <div className={"items-center max-w-xl"}>
-                            <InputLabel for={"id_cliente"} value="ID Cliente"/>
-                            <TextInput type="text" name="id_cliente" id={"id_cliente"} className={"block w-full mt-1 py-1"}
-                                       value={prontuario.data[0].cpf_cliente} disabled>
+                            <InputLabel for={"cpf_cliente"} value="CPF do Cliente"/>
+                            <TextInput type="text" name="cpf_cliente" id={"cpf_cliente"} className={"block w-full mt-1 py-1"}
+                                       value={prontuario.cpf_cliente} disabled>
                             </TextInput>
                             <InputError />
                         </div>
                         <div className={"max-w-xl items-center"}>
-                            <InputLabel for="id_especialidade" value="ID Especialidade" />
-                            <TextInput type="text" name="id_especiliadade" id="{id_especialidade}" className={"block w-full mt-1 py-1"}
-                                       value={prontuario.data[0].id_especialidade} disabled/>
+                            <InputLabel for="especialidade" value="Atendimento" />
+                            <TextInput type="text" name="id_especiliadade" id="{especialidade}" className={"block w-full mt-1 py-1"}
+                                       value={prontuario.nome_especialidade} disabled/>
                             <InputError />
                         </div>
                     </div>
                     <div className={"md:grid md:grid-cols-3 md:gap-4 mt-3 flex flex-col justify-center"}>
                         <div className={"items-center max-w-xl"}>
-                            <InputLabel for="id_profissional" value="Identificador do Profissional" />
+                            <InputLabel for="" value="Identificador do Profissional" />
                             <TextInput type="text" name="{id_cliente}" className={"block w-full mt-1 py-1"}
-                                       value={prontuario.data[0].id_profissional_de_saude} disabled/>
+                                       value={prontuario.nome_profissional} disabled/>
                         </div>
                         <div className={"items-center max-w-xs md:px-4 md:mx-4"}>
                             <InputLabel for="id_data" value="Data"/>
-                            <TextInput type={"prontuario.data[0].data_hora" ?? "date"} name="id_data" className={"block w-full mt-1 py-1"}
-                                       value={prontuario.data[0].data_abertura} disabled/>
+                            <TextInput type={"prontuario.data_hora" ?? "date"} name="id_data" className={"block w-full mt-1 mr-2 px-2 py-1"}
+                                       value={prontuario.data_abertura} disabled/>
                         </div>
                         <div className={"items-center max-w-full"}>
                             <InputLabel for="link" value="RIS e PACS" />
