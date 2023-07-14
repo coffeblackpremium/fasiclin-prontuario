d26f1b6014593b3f31930505fc4af895d5922fe0
João Damaceno <joaodamaceno188@gmail.com>
2023-07-14 18:14:57 -0400

    fix: :truck: renomeia funcao route para rotas de procedimentos.

--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -29,7 +29,7 @@ export const Navbar = () => {
                                aria-current="page">Home</Link>
                         </li>
                         <li>
-                            <Link href={route('prontuario.index')}
+                            <Link href={route('procedimentos.index')}
                                   className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
                                   aria-current="page">Adicionar Procedimentos</Link>
                         </li>
